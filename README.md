d1ce9b37e6a428d6ce1aa4522dbe0904cffed603
hugohil <hugo@soixantecircuits.fr>
2015-10-01 18:53:12 +0200

    Merge refactored version in old version + update build + clean up

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-## ElementTransitions
+# ElementTransitions
 
 **Fork under active development. For any question, contact @hugohil.**
+
+## Installation
 
 ##### License
 The CSS animations in elementTransitions.css were written by Codrops and therefore fall under [their license](http://tympanus.net/codrops/licensing/).  All other source code is released under the MIT License.
--- src/js/elementTransitions.js
+++ src/js/elementTransitions.js
@@ -1,133 +1,178 @@
-var PageTransitions = (function($) {
-  var startElement = 0,
-  animEndEventNames = {
-    'WebkitAnimation': 'webkitAnimationEnd',
-    'OAnimation': 'oAnimationEnd',
-    'msAnimation': 'MSAnimationEnd',
-    'animation': 'animationend'
+/**
+ * ElementTransitions.js
+ * Initially by @dan-silver
+ * Re-mastered version by @hugohil
+ *
+ * @todo Support an autoload attribute
+ */
+var ElementTransitions = function(){
+  var self = this;
+
+  /**
+   * [init description]
+   * @return {void}
+   */
+  function init (){
+    self.animationEndEventName = getAnimationEndVendorName();
+    self.supportedEvents = ['click', 'touchend'];
+
+    var blocks = document.querySelectorAll('.et-block');
+    for (var i = 0; i < blocks.length; i++) {
+      blocks[i].dataset.originalClassList = blocks[i].className;
+    };
+
+    var wrappers = document.querySelectorAll(".et-wrapper");
+    for (var i = 0; i < wrappers.length; i++) {
+      var wrapper = wrappers[i];
+      wrapper.dataset.current = 0;
+      toggleClass(wrapper.querySelectorAll('.et-block')[wrapper.dataset.current], 'et-block-current');
+      wrapper.dataset.isAnimating = false;
+
+      var trigger = (wrapper.className.indexOf('et-trigger') > -1) ? wrapper : wrapper.querySelector('.et-trigger');
+      initializeAnimations(trigger, wrapper);
+    }
   }
 
-  function getTransitionPrefix() {
-    var b = document.body || document.documentElement;
-    var s = b.style;
-    var p = 'animation';
-    if(typeof s[p] == 'string')
-      return 'animation';
+  /**
+   * Initialize event handlers and animations.
+   * @param  {HTMLElement} trigger The HTML element on which you click to trigger animations.
+   * @param  {HTMLElement} wrapper The HTML element containing the blocks (may be the same as trigger).
+   * @return {void}
+   */
+  function initializeAnimations (trigger, wrapper){
+    addMultipleEventsListener(trigger, self.supportedEvents, function (event){
+      // Not sure if the `event.preventDefault()` is a good idea. Need to cancel double events on touch devices.
+      // I'm open to suggestions: ping @hugohil pretty much anywhere :)
+      event.preventDefault();
+      animate(wrapper, trigger);
+    });
+  }
 
-    // Tests for vendor specific prop
-    var v = ['Moz', 'Webkit', 'Khtml', 'O', 'ms'],
-    p = p.charAt(0).toUpperCase() + p.substr(1);
-    for( var i=0; i<v.length; i++ ) {
-      if(typeof s[v[i] + p] == 'string')
-        return v[i] + p;
+  /**
+   * Acutally animate the element
+   * @param  {HTMLElement} trigger The HTML element on which you clicked to trigger animations.
+   * @param  {HTMLElement} wrapper The HTML element containing the blocks (may be the same as trigger).
+   * @return {void}
+   */
+  function animate(wrapper, trigger){
+    var step = trigger.getAttribute('et-step') || 1,
+        blocks = wrapper.querySelectorAll('.et-block'),
+        current = wrapper.dataset.current,
+        currentBlock = blocks[current];
+
+    if(wrapper.dataset.isAnimating == 'true'){
+      return false;
+    }
+    wrapper.dataset.isAnimating = true;
+
+    var outClass = formatClass(trigger.getAttribute('et-out')),
+        inClass = formatClass(trigger.getAttribute('et-in'));
+
+    for (var i = 0; i < outClass.length; i++) {
+      toggleClass(currentBlock, outClass[i]);
+    }
+
+    currentBlock.addEventListener(self.animationEndEventName, function (event){
+      currentBlock.removeEventListener(self.animationEndEventName, arguments.callee);
+
+      // Switch current block
+      var prevBlock = currentBlock;
+      current = ((Number(current) + step) > blocks.length - 1) ? 0 : Number(current) + step;
+      wrapper.dataset.current = current;
+      currentBlock = blocks[current];
+
+      for (var i = 0; i < inClass.length; i++) {
+        toggleClass(currentBlock, inClass[i]);
+        toggleClass(currentBlock, 'et-block-current');
+      }
+
+      currentBlock.addEventListener(self.animationEndEventName, function (event){
+        currentBlock.removeEventListener(self.animationEndEventName, arguments.callee);
+
+        prevBlock.className = prevBlock.dataset.originalClassList;
+        currentBlock.className = currentBlock.dataset.originalClassList + ' et-block-current';
+
+        wrapper.dataset.isAnimating = false;
+      });
+    });
+  }
+
+  /**
+   * Utilities functions
+   */
+
+  /**
+   * Browser may have vendor prefix for the animationend event
+   * @link https://developer.mozilla.org/en-US/docs/Web/Events/animationend
+   * @return {String} The browser animationend event name.
+   */
+  function getAnimationEndVendorName(){
+    var postfix = 'Animation';
+    var animationEndEventNames = {
+      'WebkitAnimation': 'webkitAnimationEnd',
+      'OAnimation': 'oAnimationEnd',
+      'msAnimation': 'MSAnimationEnd',
+      'animation': 'animationend'
+    }
+
+    if(typeof document.body.style[postfix] == 'string'){
+      return animationEndEventNames[postfix.toLowerCase()];
+    }
+
+    var vendors = ['Moz', 'Webkit', 'Khtml', 'O', 'ms'];
+    for(var i = 0; i < vendors.length; i++){
+      if(typeof document.body.style[vendors[i] + postfix] == 'string'){
+        return animationEndEventNames[vendors[i] + postfix];
+      }
     }
     return false;
   }
-  // animation end event name
-  animEndEventName = animEndEventNames[getTransitionPrefix()];
 
-  function init() {
-    $(".et-page").each(function() {
-      $(this).data('originalClassList', $(this).attr('class'));
-    });
-    $(".et-wrapper").each(function() {
-      $(this).data('current', 0);
-      $(this).data('isAnimating', false);
-      $(this).children(".et-page").eq(startElement).addClass('et-page-current');
-    });
-
-    $(".et-rotate").click(function() {
-      animate($(this));
-    });
-  }
-
-  function animate(block, callback) {
-    var outClass = formatClass($(block).attr('et-out')),
-        inClass  = formatClass($(block).attr('et-in')),
-        step     = $(block).attr('et-step'),
-        block    = $(block).closest('.et-wrapper')
-
-    if (step === undefined)
-      step = 1;
-
-    block = $(block);
-    var current = block.data('current'),
-        $pages = block.children('.et-page'),
-        pagesCount = $pages.length,
-        endCurrPage = false,
-        endNextPage = false;
-
-    if(block.data('isAnimating')) {
-      return false;
-    }
-
-    block.data('isAnimating', true);
-
-    var $currPage = $pages.eq(current);
-    current = current*1 + step*1;
-    if (current >= pagesCount) {
-      current=0;
-    }
-
-    block.data('current', current);
-
-    var $nextPage = $pages.eq(current);
-
-    outClass.forEach(function(c) {
-      $currPage.addClass(c);
-    });
-
-    $currPage.on(animEndEventName, function() {
-      $currPage.off(animEndEventName);
-      endCurrPage = true;
-      if(endNextPage) {
-        if(jQuery.isFunction(callback)) {
-          callback(block, $nextPage, $currPage);
-        }
-        onEndAnimation($currPage, $nextPage, block);
-      }
-    });
-
-    inClass.forEach(function(c) {
-      $nextPage.addClass(c);
-    });
-    $nextPage.addClass("et-page-current");
-
-    $nextPage.on(animEndEventName, function() {
-      $nextPage.off(animEndEventName);
-      endNextPage = true;
-      if(endCurrPage) {
-        onEndAnimation($currPage, $nextPage, block);
-      }
-    });
-  }
-
-  function onEndAnimation($outpage, $inpage, block) {
-    resetPage($outpage, $inpage);
-    $outpage.trigger("animation.out.complete");
-    $inpage.trigger("animation.in.complete");
-    block.data('isAnimating', false);
-  }
-
-  function resetPage($outpage, $inpage) {
-    $outpage.attr('class', $outpage.data( 'originalClassList'));
-    $inpage.attr('class', $inpage.data( 'originalClassList') + ' et-page-current');
-  }
-
-  function formatClass(str) {
-    var classes = str.split(" ");
+  /**
+   * Will prefix input to match CSS animation classes.
+   * @param  {String} input The string to be concatenate with animation class prefix.
+   * @return {void}
+   */
+  function formatClass (input) {
     var output = [];
-    for(var i=0; i<classes.length; i++){
-      output.push("pt-page-" + classes[i]);
+    var classes = input.split(/\s/);
+    for(var i = 0; i < classes.length; i++){
+      output.push('et-' + classes[i]);
     }
     return output;
   }
-  return {
-    init : init,
-    animate: animate
-  };
-})(jQuery);
 
-jQuery(function($) {
-  PageTransitions.init();
-});
+  /**
+   * This binds multiples event listeners to a single HTML element.
+   * @param {HTMLElement} element The element to bind the events.
+   * @param {Events[]} events An array of Events.
+   * @param {Function} handler The function to be called when one of the events is triggered.
+   * @return {void}
+   */
+  function addMultipleEventsListener (element, events, handler){
+    for (var i = 0; i < events.length; i++) {
+      element.addEventListener(events[i], function (event){
+        handler(event);
+      });
+    }
+  }
+
+  /**
+   * Toggle a class on a single HTML element.
+   * @param  {HTMLElement} element The element to modify.
+   * @param  {String} classname The class to be toggled.
+   * @return {void}
+   */
+  function toggleClass(element, classname){
+    var re = new RegExp(classname, 'gi');
+    element.className = (element.className.indexOf(classname) > -1) ? element.className.replace(re, '') : element.className + ' ' + classname;
+  }
+
+  return {
+    init: init,
+    animate: animate
+  }
+}
+
+var elementTransitions = ElementTransitions();
+elementTransitions.init();
